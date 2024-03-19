27851b177a137071e57a5860cf7ebde6fa4d1d8c
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2024-03-19 20:15:18 +0900

    JPARepository 맛보기?

--- src/test/java/shop/mtcoding/blog/board/BoardJPARepositoryTest.java
+++ src/test/java/shop/mtcoding/blog/board/BoardJPARepositoryTest.java
@@ -1,13 +1,82 @@
 package shop.mtcoding.blog.board;
 
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Sort;
+import shop.mtcoding.blog.user.User;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.SortedMap;
 
 @DataJpaTest
 public class BoardJPARepositoryTest {
+    @Autowired
     private BoardJPARepository boardJPARepository;
+    @Autowired
+    private EntityManager em;
+
+    @Test
+    public void deleteById_test() {
+        //given
+        int id = 1;
+
+        // when
+        boardJPARepository.deleteById(id);
+
+        // then
+        em.flush();
+
+    }
+
+    @Test
+    public void findAll_test() {
+        //given
+        Sort sort = Sort.by(Sort.Direction.DESC, "id");
+
+        // when
+        List<Board> boardList = boardJPARepository.findAll(sort);
+
+        // then
+        System.out.println("findAll_test: " + boardList.size());
+
+    }
+
+    @Test
+    public void findById_test() {
+        //given
+        int id = 1;
+
+        // when
+        Optional<Board> boardOP = boardJPARepository.findById(id);
+
+        // then
+        if (boardOP.isPresent()) {
+            Board board = boardOP.get();
+            System.out.println("findById_test: " + board);
+        }
+
+    }
+
+    @Test
+    public void save_test() {
+        //given
+        User sessionUser = User.builder().id(1).build();
+        Board board = Board.builder()
+                .title("title5")
+                .content("content5")
+                .user(sessionUser)
+                .build();
+
+        // when
+        boardJPARepository.save(board);
+
+        // then
+        System.out.println("save_test: " + board);
+
+    }
 
     @Test
     public void findByIdJoinUser_test() {
@@ -17,7 +86,7 @@ public class BoardJPARepositoryTest {
         // when
         Optional<Board> boardOP = boardJPARepository.findByIdJoinUser(id);
 
-            System.out.println("findByIdJoinUser_test: " + boardOP);
+        System.out.println("findByIdJoinUser_test: " + boardOP);
         // then
 
     }
--- src/test/java/shop/mtcoding/blog/user/UserJPARepositoryTest.java
+++ src/test/java/shop/mtcoding/blog/user/UserJPARepositoryTest.java
@@ -13,6 +13,36 @@ public class UserJPARepositoryTest {
     private UserJPARepository userJPARepository;
 
     @Test
+    public void save_test() {
+        //given
+        User user = User.builder()
+                .username("happy")
+                .password("1234")
+                .email("happy@nate.com")
+                .build();
+        // when
+        userJPARepository.save(user);
+        // then
+        System.out.println("save_test: " + user);
+    }
+
+    @Test
+    public void findById_test() {
+        //given
+        int id = 1;
+
+        // when
+        Optional<User> userOP = userJPARepository.findById(id);
+
+        // then
+        if (userOP.isPresent()) {
+            User user = userOP.get();
+            System.out.println("findById_test: " + user.getEmail());
+        }
+
+    }
+
+    @Test
     public void findByUsernameAndPassword_test() {
         // given
         String username = "ssar";
